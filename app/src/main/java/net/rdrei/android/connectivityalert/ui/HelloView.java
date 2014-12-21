e4c39f3b46ccc6abbba1296a74144a003613fda8
Pascal Hartig <passy@twitter.com>
2014-12-21 22:56:29 +0100

    Boo, Crash!

---
+++ app/src/main/java/net/rdrei/android/connectivityalert/ui/HelloView.java
@@ -0,0 +1,40 @@
+package net.rdrei.android.connectivityalert.ui;
+
+import android.app.ActionBar;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import net.rdrei.android.connectivityalert.MainActivity;
+
+import javax.inject.Inject;
+
+public class HelloView extends TextView {
+    @Inject
+    ActionBar mActionBar;
+
+    public HelloView(final Context context) {
+        super(context);
+    }
+
+    public HelloView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public HelloView(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public HelloView(final Context context, final AttributeSet attrs, final int defStyleAttr, final int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        ((MainActivity) getContext()).inject(this);
+        setText("Hello, World!");
+        mActionBar.setTitle("HelloView");
+    }
+}
